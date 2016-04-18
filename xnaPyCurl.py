c1327835ba06548bc74f9d0cdcf77d9f7a9d6342
rushton <rushtongarth@users.noreply.github.com>
2016-04-17 19:24:28 -0500

    Update xnaPyCurl.py

--- xnaPyCurl.py
+++ xnaPyCurl.py
@@ -4,6 +4,10 @@ import json,pycurl
 from cStringIO import StringIO as StringIO
 
 class xnaPyCurl(object):
+	"""xnaPyCurl: Connect and pull resources from XNAT
+	
+	Class requires an input url to the XNAT API from which 
+	resources are to be pulled"""
 	def __init__(self,basepage):
 		self.c = pycurl.Curl()
 		self.setopt = self.c.setopt
@@ -17,6 +21,10 @@ class xnaPyCurl(object):
 		elif mode=='ses':
 			return '{base}/JSESSION'
 	def login(self,*creds):
+		"""login method
+		
+		requires user credentials as input as username,password pair
+		gets a session cookie for future actions"""
 		if self.buf.tell()!=0:
 			self.buf.reset()
 		uri = self.ses.format(base=self.basepage)
@@ -29,8 +37,16 @@ class xnaPyCurl(object):
 		self.c.reset()
 		return 1
 	def logout(self):
+		"""logout method
+		
+		destroys previously acquired cookie attribute."""
 		if self.buf.tell()!=0:
 			self.buf.reset()
+		if not hasattr(self,'cookie'):
+			self.buf.write('')
+			self.buf.truncate()
+			self.buf.close()
+			return ''
 		uri = self.ses.format(base=self.basepage)
 		self.setopt(pycurl.URL,uri)
 		self.setopt(pycurl.COOKIE, "JSESSIONID=%s"%self.cookie)
