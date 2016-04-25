4121e5c1cb74d6084d4cf2766a07858a1669adf8
rushton <rushtongarth@users.noreply.github.com>
2016-04-25 18:01:25 -0500

    Update single.py

--- single.py
+++ single.py
@@ -2,7 +2,7 @@
 
 import json,pycurl,operator as op
 from collections import OrderedDict as OD
-from cStringIO import StringIO as StringIO
+from cStringIO import StringIO as SIO
 
 
 
@@ -12,7 +12,7 @@ class CurlQuery(object):
 		self.setopt = self.c.setopt
 		b = page[:-1] if page.endswith('/') else page
 		self.basepage = b if b.endswith('REST') else b+'/REST'
-		self.buf = StringIO()
+		self.buf = SIO()
 		self.logged = False
 	def getfromuri(self,uri):
 		if self.buf.tell()!=0:
@@ -24,37 +24,42 @@ class CurlQuery(object):
 		self.buf.truncate()
 		return json.loads(self.buf.getvalue())
 
-		
 	def login(self,*creds,**kwargs):
-		cxns = kwargs.get('cxncount',0)
 		if not self.logged:
 			self.c = pycurl.Curl()
 			self.setopt = self.c.setopt
-			self.buf = StringIO()
+			self.buf = SIO()
 		if self.buf.tell()!=0:
 			self.buf.reset()
-		uri = self.ses.format(base=self.basepage)
+		uri = '{base}/JSESSION'.format(base=self.basepage)
 		self.setopt(pycurl.URL,uri)
 		self.setopt(pycurl.WRITEDATA, self.buf)
 		self.setopt(pycurl.USERPWD,'{0}:{1}'.format(*creds))
-		if cxns:
-			jar = []
-			for i in xrange(cxns):
-				self.c.perform()
-				tmpcookie = self.buf.getvalue()
-				if len(tmpcookie)==32:
-					jar.append(tmpcookie)
-				self.buf.reset()
-				self.buf.truncate()
-			self.cookies = jar[:]
+		self.c.perform()
+		self.cookie = self.buf.getvalue()
+		self.c.reset()
+		if len(self.cookie)==32:
 			self.logged = True
-			return len(self.cookies)
-		else: 
-			self.c.perform()
-			self.cookie = self.buf.getvalue()
-			self.c.reset()
-			if len(self.cookie)==32:
-				self.logged = True
-				return 1
-		return 0
+			return 1
+		else:
+			return 0
+
+	def logout(self):
+		if self.buf.tell()!=0:
+			self.buf.reset()
+		uri = '{base}/JSESSION'.format(base=self.basepage)
+		self.setopt(pycurl.URL, uri)
+		body = ''
+		self.setopt(pycurl.COOKIE, "JSESSIONID=%s"%self.cookie)
+		self.setopt(pycurl.CUSTOMREQUEST, 'DELETE')
+		self.setopt(pycurl.WRITEDATA, self.buf)
+		self.c.perform()
+		self.buf.truncate()
+		body = self.buf.getvalue()
+		self.c.close()
+		self.logged = False
+		self.buf.close()
+		return body
+
 		
+	
